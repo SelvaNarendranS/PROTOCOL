88cd2b4756d48864a7c5ddba3ad33963336ff643
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-03-29 23:25:28 +0530

    added 4 mode operation SPI output

---
+++ OFF_CHIP_PROTOCOLS/SPI/SPI_4_MODES/06.output.v
@@ -0,0 +1,104 @@
+VCD info: dumpfile dump.vcd opened for output.
+
+ [0] ---------------- reset -----------
+  [0] -- spi_en = 0 | SCLK = 0 | cs = 1 | MISO = z | MOSI = 0 | master_out = 0(00000000) | slave_out = 0(00000000) -- ms_st = 0 | slv_st = 0
+ [100] ---------------- reset released -----------
+		----------- NEXT DATA -----------------
+  [120] - master datain = 230(11100110) | slave datain = 146(10010010) | MODE = MODE1
+ --- spi enable done ---
+  [120] -- spi_en = 1 | SCLK = 0 | cs = 1 | MISO = z | MOSI = 0 | master_out = 0(00000000) | slave_out = 0(00000000) -- ms_st = 0 | slv_st = 0
+  [125] -- spi_en = 1 | SCLK = 0 | cs = 1 | MISO = z | MOSI = 0 | master_out = 0(00000000) | slave_out = 0(00000000) -- ms_st = 1 | slv_st = 0
+  [135] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 0 | MOSI = 0 | master_out = 0(00000000) | slave_out = 0(00000000) -- ms_st = 2 | slv_st = 0
+  [145] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 1 | MOSI = 0 | master_out = 0(00000000) | slave_out = 0(00000000) -- ms_st = 2 | slv_st = 1
+  [195] -- spi_en = 0 | SCLK = 1 | cs = 0 | MISO = 1 | MOSI = 0 | master_out = 0(00000000) | slave_out = 0(00000000) -- ms_st = 2 | slv_st = 1
+  [205] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 1 | MOSI = 1 | master_out = 0(00000000) | slave_out = 0(00000000) -- ms_st = 2 | slv_st = 1
+  [215] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 0 | MOSI = 1 | master_out = 0(00000000) | slave_out = 0(00000000) -- ms_st = 2 | slv_st = 1
+  [295] -- spi_en = 0 | SCLK = 1 | cs = 0 | MISO = 0 | MOSI = 1 | master_out = 0(00000000) | slave_out = 0(00000000) -- ms_st = 2 | slv_st = 1
+  [305] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 0 | MOSI = 1 | master_out = 0(00000000) | slave_out = 0(00000000) -- ms_st = 2 | slv_st = 1
+  [395] -- spi_en = 0 | SCLK = 1 | cs = 0 | MISO = 0 | MOSI = 1 | master_out = 0(00000000) | slave_out = 0(00000000) -- ms_st = 2 | slv_st = 1
+  [405] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 0 | MOSI = 1 | master_out = 0(00000000) | slave_out = 0(00000000) -- ms_st = 2 | slv_st = 1
+  [415] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 1 | MOSI = 1 | master_out = 0(00000000) | slave_out = 0(00000000) -- ms_st = 2 | slv_st = 1
+  [495] -- spi_en = 0 | SCLK = 1 | cs = 0 | MISO = 1 | MOSI = 1 | master_out = 0(00000000) | slave_out = 0(00000000) -- ms_st = 2 | slv_st = 1
+  [505] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 1 | MOSI = 0 | master_out = 0(00000000) | slave_out = 0(00000000) -- ms_st = 2 | slv_st = 1
+  [515] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 0 | MOSI = 0 | master_out = 0(00000000) | slave_out = 0(00000000) -- ms_st = 2 | slv_st = 1
+  [595] -- spi_en = 0 | SCLK = 1 | cs = 0 | MISO = 0 | MOSI = 0 | master_out = 0(00000000) | slave_out = 0(00000000) -- ms_st = 2 | slv_st = 1
+  [605] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 0 | MOSI = 0 | master_out = 0(00000000) | slave_out = 0(00000000) -- ms_st = 2 | slv_st = 1
+  [695] -- spi_en = 0 | SCLK = 1 | cs = 0 | MISO = 0 | MOSI = 0 | master_out = 0(00000000) | slave_out = 0(00000000) -- ms_st = 2 | slv_st = 1
+  [705] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 0 | MOSI = 1 | master_out = 0(00000000) | slave_out = 0(00000000) -- ms_st = 2 | slv_st = 1
+  [715] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 1 | MOSI = 1 | master_out = 0(00000000) | slave_out = 0(00000000) -- ms_st = 2 | slv_st = 1
+  [795] -- spi_en = 0 | SCLK = 1 | cs = 0 | MISO = 1 | MOSI = 1 | master_out = 0(00000000) | slave_out = 0(00000000) -- ms_st = 2 | slv_st = 1
+  [805] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 1 | MOSI = 1 | master_out = 0(00000000) | slave_out = 0(00000000) -- ms_st = 2 | slv_st = 1
+  [815] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 0 | MOSI = 1 | master_out = 0(00000000) | slave_out = 0(00000000) -- ms_st = 2 | slv_st = 1
+  [895] -- spi_en = 0 | SCLK = 1 | cs = 0 | MISO = 0 | MOSI = 1 | master_out = 0(00000000) | slave_out = 0(00000000) -- ms_st = 2 | slv_st = 1
+  [905] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 0 | MOSI = 0 | master_out = 0(00000000) | slave_out = 0(00000000) -- ms_st = 2 | slv_st = 1
+  [915] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 0 | MOSI = 0 | master_out = 0(00000000) | slave_out = 0(00000000) -- ms_st = 3 | slv_st = 1
+  [925] -- spi_en = 0 | SCLK = 0 | cs = 1 | MISO = z | MOSI = 0 | master_out = 146(10010010) | slave_out = 0(00000000) -- ms_st = 0 | slv_st = 2
+  [935] -- spi_en = 0 | SCLK = 0 | cs = 1 | MISO = z | MOSI = 0 | master_out = 146(10010010) | slave_out = 230(11100110) -- ms_st = 0 | slv_st = 0
+  [945] --- spi transmission and receive done ---
+	------------------------------------------------------------
+			 SUCCESS: Received master dataout = 146 | slave dataout = 230 correctly
+	--------------------------------------------------------------
+
+		----------- NEXT DATA -----------------
+  [980] - master datain = 179(10110011) | slave datain = 56(00111000) | MODE = MODE1
+ --- spi enable done ---
+  [980] -- spi_en = 1 | SCLK = 0 | cs = 1 | MISO = z | MOSI = 0 | master_out = 146(10010010) | slave_out = 230(11100110) -- ms_st = 0 | slv_st = 0
+  [985] -- spi_en = 1 | SCLK = 0 | cs = 1 | MISO = z | MOSI = 0 | master_out = 146(10010010) | slave_out = 230(11100110) -- ms_st = 1 | slv_st = 0
+  [995] -- spi_en = 0 | SCLK = 1 | cs = 0 | MISO = 0 | MOSI = 0 | master_out = 146(10010010) | slave_out = 230(11100110) -- ms_st = 2 | slv_st = 0
+  [1005] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 0 | MOSI = 1 | master_out = 146(10010010) | slave_out = 230(11100110) -- ms_st = 2 | slv_st = 1
+  [1095] -- spi_en = 0 | SCLK = 1 | cs = 0 | MISO = 0 | MOSI = 1 | master_out = 146(10010010) | slave_out = 230(11100110) -- ms_st = 2 | slv_st = 1
+  [1105] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 0 | MOSI = 0 | master_out = 146(10010010) | slave_out = 230(11100110) -- ms_st = 2 | slv_st = 1
+  [1115] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 1 | MOSI = 0 | master_out = 146(10010010) | slave_out = 230(11100110) -- ms_st = 2 | slv_st = 1
+  [1195] -- spi_en = 0 | SCLK = 1 | cs = 0 | MISO = 1 | MOSI = 0 | master_out = 146(10010010) | slave_out = 230(11100110) -- ms_st = 2 | slv_st = 1
+  [1205] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 1 | MOSI = 1 | master_out = 146(10010010) | slave_out = 230(11100110) -- ms_st = 2 | slv_st = 1
+  [1295] -- spi_en = 0 | SCLK = 1 | cs = 0 | MISO = 1 | MOSI = 1 | master_out = 146(10010010) | slave_out = 230(11100110) -- ms_st = 2 | slv_st = 1
+  [1305] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 1 | MOSI = 1 | master_out = 146(10010010) | slave_out = 230(11100110) -- ms_st = 2 | slv_st = 1
+  [1395] -- spi_en = 0 | SCLK = 1 | cs = 0 | MISO = 1 | MOSI = 1 | master_out = 146(10010010) | slave_out = 230(11100110) -- ms_st = 2 | slv_st = 1
+  [1405] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 1 | MOSI = 0 | master_out = 146(10010010) | slave_out = 230(11100110) -- ms_st = 2 | slv_st = 1
+  [1415] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 0 | MOSI = 0 | master_out = 146(10010010) | slave_out = 230(11100110) -- ms_st = 2 | slv_st = 1
+  [1495] -- spi_en = 0 | SCLK = 1 | cs = 0 | MISO = 0 | MOSI = 0 | master_out = 146(10010010) | slave_out = 230(11100110) -- ms_st = 2 | slv_st = 1
+  [1505] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 0 | MOSI = 0 | master_out = 146(10010010) | slave_out = 230(11100110) -- ms_st = 2 | slv_st = 1
+  [1595] -- spi_en = 0 | SCLK = 1 | cs = 0 | MISO = 0 | MOSI = 0 | master_out = 146(10010010) | slave_out = 230(11100110) -- ms_st = 2 | slv_st = 1
+  [1605] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 0 | MOSI = 1 | master_out = 146(10010010) | slave_out = 230(11100110) -- ms_st = 2 | slv_st = 1
+  [1695] -- spi_en = 0 | SCLK = 1 | cs = 0 | MISO = 0 | MOSI = 1 | master_out = 146(10010010) | slave_out = 230(11100110) -- ms_st = 2 | slv_st = 1
+  [1705] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 0 | MOSI = 1 | master_out = 146(10010010) | slave_out = 230(11100110) -- ms_st = 2 | slv_st = 1
+  [1715] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 0 | MOSI = 1 | master_out = 146(10010010) | slave_out = 230(11100110) -- ms_st = 3 | slv_st = 1
+  [1725] -- spi_en = 0 | SCLK = 0 | cs = 1 | MISO = z | MOSI = 0 | master_out = 56(00111000) | slave_out = 230(11100110) -- ms_st = 0 | slv_st = 2
+  [1735] -- spi_en = 0 | SCLK = 0 | cs = 1 | MISO = z | MOSI = 0 | master_out = 56(00111000) | slave_out = 179(10110011) -- ms_st = 0 | slv_st = 0
+  [1745] --- spi transmission and receive done ---
+	------------------------------------------------------------
+			 SUCCESS: Received master dataout = 56 | slave dataout = 179 correctly
+	--------------------------------------------------------------
+
+		----------- NEXT DATA -----------------
+  [1780] - master datain = 15(00001111) | slave datain = 7(00000111) | MODE = MODE1
+ --- spi enable done ---
+  [1780] -- spi_en = 1 | SCLK = 0 | cs = 1 | MISO = z | MOSI = 0 | master_out = 56(00111000) | slave_out = 179(10110011) -- ms_st = 0 | slv_st = 0
+  [1785] -- spi_en = 1 | SCLK = 0 | cs = 1 | MISO = z | MOSI = 0 | master_out = 56(00111000) | slave_out = 179(10110011) -- ms_st = 1 | slv_st = 0
+  [1795] -- spi_en = 0 | SCLK = 1 | cs = 0 | MISO = 0 | MOSI = 0 | master_out = 56(00111000) | slave_out = 179(10110011) -- ms_st = 2 | slv_st = 0
+  [1805] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 0 | MOSI = 0 | master_out = 56(00111000) | slave_out = 179(10110011) -- ms_st = 2 | slv_st = 1
+  [1895] -- spi_en = 0 | SCLK = 1 | cs = 0 | MISO = 0 | MOSI = 0 | master_out = 56(00111000) | slave_out = 179(10110011) -- ms_st = 2 | slv_st = 1
+  [1905] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 0 | MOSI = 0 | master_out = 56(00111000) | slave_out = 179(10110011) -- ms_st = 2 | slv_st = 1
+  [1995] -- spi_en = 0 | SCLK = 1 | cs = 0 | MISO = 0 | MOSI = 0 | master_out = 56(00111000) | slave_out = 179(10110011) -- ms_st = 2 | slv_st = 1
+  [2005] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 0 | MOSI = 0 | master_out = 56(00111000) | slave_out = 179(10110011) -- ms_st = 2 | slv_st = 1
+  [2095] -- spi_en = 0 | SCLK = 1 | cs = 0 | MISO = 0 | MOSI = 0 | master_out = 56(00111000) | slave_out = 179(10110011) -- ms_st = 2 | slv_st = 1
+  [2105] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 0 | MOSI = 0 | master_out = 56(00111000) | slave_out = 179(10110011) -- ms_st = 2 | slv_st = 1
+  [2195] -- spi_en = 0 | SCLK = 1 | cs = 0 | MISO = 0 | MOSI = 0 | master_out = 56(00111000) | slave_out = 179(10110011) -- ms_st = 2 | slv_st = 1
+  [2205] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 0 | MOSI = 1 | master_out = 56(00111000) | slave_out = 179(10110011) -- ms_st = 2 | slv_st = 1
+  [2215] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 1 | MOSI = 1 | master_out = 56(00111000) | slave_out = 179(10110011) -- ms_st = 2 | slv_st = 1
+  [2295] -- spi_en = 0 | SCLK = 1 | cs = 0 | MISO = 1 | MOSI = 1 | master_out = 56(00111000) | slave_out = 179(10110011) -- ms_st = 2 | slv_st = 1
+  [2305] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 1 | MOSI = 1 | master_out = 56(00111000) | slave_out = 179(10110011) -- ms_st = 2 | slv_st = 1
+  [2395] -- spi_en = 0 | SCLK = 1 | cs = 0 | MISO = 1 | MOSI = 1 | master_out = 56(00111000) | slave_out = 179(10110011) -- ms_st = 2 | slv_st = 1
+  [2405] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 1 | MOSI = 1 | master_out = 56(00111000) | slave_out = 179(10110011) -- ms_st = 2 | slv_st = 1
+  [2495] -- spi_en = 0 | SCLK = 1 | cs = 0 | MISO = 1 | MOSI = 1 | master_out = 56(00111000) | slave_out = 179(10110011) -- ms_st = 2 | slv_st = 1
+  [2505] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 1 | MOSI = 1 | master_out = 56(00111000) | slave_out = 179(10110011) -- ms_st = 2 | slv_st = 1
+  [2515] -- spi_en = 0 | SCLK = 0 | cs = 0 | MISO = 0 | MOSI = 1 | master_out = 56(00111000) | slave_out = 179(10110011) -- ms_st = 3 | slv_st = 1
+  [2525] -- spi_en = 0 | SCLK = 0 | cs = 1 | MISO = z | MOSI = 0 | master_out = 7(00000111) | slave_out = 179(10110011) -- ms_st = 0 | slv_st = 2
+  [2535] -- spi_en = 0 | SCLK = 0 | cs = 1 | MISO = z | MOSI = 0 | master_out = 7(00000111) | slave_out = 15(00001111) -- ms_st = 0 | slv_st = 0
+  [2545] --- spi transmission and receive done ---
+	------------------------------------------------------------
+			 SUCCESS: Received master dataout = 7 | slave dataout = 15 correctly
+	--------------------------------------------------------------
+
+testbench.sv:80: $finish called at 2655 (1s)
+Done
