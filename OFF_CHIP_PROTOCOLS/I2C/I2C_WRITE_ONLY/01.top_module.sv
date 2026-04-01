23c6140c71966989e582b0841cf1ac5d295921e8
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-04-01 23:57:14 +0530

    i2c write only top module -- i2c bus

---
+++ OFF_CHIP_PROTOCOLS/I2C/I2C_WRITE_ONLY/01.top_module.sv
@@ -0,0 +1,84 @@
+// I2C top module -- only for write
+`include "clock_divider1.sv"
+`include "master1.sv"
+`include "slave1.sv"
+
+module i2c_top_module #(
+  parameter SYS_CLK 	  = 50_000_000,		// hz - in hertz(mhz)
+  parameter I2C_CLK 	  = 100_000,		// hz - in hertz(mhz)
+  parameter ADDRESS_WIDTH = 7,
+  parameter DATA_WIDTH    = 8,
+  parameter SLAVE_ADDR 	  = 32)				// fixed lave address
+  (
+    // system
+    input 					  clk,
+    input 					  rst,
+    
+    // control input signals
+    input 					  i2c_clk_en,	// i2c clock generator enable
+    input 					  i2c_en,		// i2c protocal enable
+    input [ADDRESS_WIDTH-1:0] addr,
+    input 					  rw,			// read - 1, write - 0 ---> read & write command
+    input [DATA_WIDTH-1:0]	  data_in,		// input data for transmission
+    
+    // result output signal
+    output 					  busy,
+    output				  	  ack_err,		// acknowledgement error - not ack
+    output 					  done,
+    output					  framing_err,
+    output [DATA_WIDTH-1:0]   data_out,		// output data -- received data
+    inout				  scl,			// serial clock
+    inout				  sda			// serial data
+  );
+  
+  // internal wire
+  wire scl_tick;
+  wire scl_clk;
+  
+  // instantation
+  
+  // clock divider
+  i2c_clock_divider #(.SYS_CLK(SYS_CLK),
+                      .I2C_CLK(I2C_CLK))
+  receiver( 
+    .clk(clk),
+    .rst(rst),
+    .i2c_clk_en(i2c_clk_en),
+    .scl_tick(scl_tick),
+    .scl_out(scl_clk)
+  );
+  
+  // master
+  i2c_master #(.ADDRESS_WIDTH(ADDRESS_WIDTH),
+               .DATA_WIDTH(DATA_WIDTH))
+  master(
+    .clk(clk),
+    .rst(rst),
+    .scl_tick(scl_tick),
+    .scl_clk(scl_clk),
+    .i2c_en(i2c_en),
+    .addr(addr),
+    .rw(rw),
+    .data_in(data_in),
+    .busy(busy),
+    .ack_err(ack_err),
+    .scl(scl),
+    .sda(sda)
+  );
+  
+  // slave
+  i2c_slave #(.ADDRESS_WIDTH(ADDRESS_WIDTH),
+              .DATA_WIDTH(DATA_WIDTH),
+              .SLAVE_ADDR(SLAVE_ADDR))
+  slave(
+    .clk(clk),
+    .rst(rst),
+    .done(done),
+    .framing_err(framing_err),
+    .data_out(data_out),
+    .scl(scl),
+    .sda(sda)
+  );
+endmodule
+
+// https://edaplayground.com/x/eFPL
